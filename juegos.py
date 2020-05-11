aa6a6bccd6a8fc5cf7c1a06ae48c47a3588c6c28
bgomez97 <bag97ve@gmail.com>
2020-05-11 19:05:19 -0300

    se agrega la funcion para agregar usuarios

--- juegos.py
+++ juegos.py
@@ -1,10 +1,26 @@
 import tictactoeModificado
 import hangman
 import reversegam
+import json
+
+def agregar_datos(nombre,opcion):
+	if opcion == '1':
+		juego_eleguido = 'ahorcado'
+	elif opcion == '2':
+		juego_eleguido = 'ta-te-ti'
+	elif opcion == '3':
+		juego_eleguido = 'otello'
+	
+	if nombre in datos:
+		datos[nombre].append(juego_eleguido)
+	else:
+		datos[nombre] = [juego_eleguido]
+	
+# def guardar_json():
+# 	json.dump(datos)
 
 
 def main(args):
-	
 	sigo_jugando = True
 	while sigo_jugando:
 		
@@ -17,14 +33,22 @@ def main(args):
 
 		opcion = input()
 		if opcion == '1':
+			nombre = input("Ingrese su nombre\n")
+			agregar_datos(nombre,'1')
 			hangman.main()
 		elif opcion == '2':
+			nombre = input("Ingrese su nombre\n")
+			agregar_datos(nombre,'2')
 			tictactoeModificado.main()
 		elif opcion == '3':
+			nombre = input("Ingrese su nombre\n")
+			agregar_datos(nombre,'3')
 			reversegam.main()
 		elif opcion == '4':
 			sigo_jugando = False
-		
+
+datos = {}
+
 if __name__ == '__main__':
-    import sys
-    sys.exit(main(sys.argv))
+	import sys
+	sys.exit(main(sys.argv))
